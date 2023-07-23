d8057b83b9036ff12cf3fcfda428dd88997f1122
bfwuw-rsrkf <zy.130606@gmail.com>
2023-07-23 13:00:20 +0600

    created Product and Woman models

--- coolsite0/women/models.py
+++ coolsite0/women/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    model = models.CharField(max_length=256)
+    price = models.FloatField(default=0)
+    color = models.CharField(max_length=256)
+    date_created = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.model
+
+
+class Woman(models.Model):
+    name = models.CharField(max_length=256)
+    age = models.IntegerField()
+    job = models.CharField(max_length=256)
+    salary = models.FloatField(default=0)
+
+    def __str__(self):
+        return self.name
