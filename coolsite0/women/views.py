dfb20c065775127d48b926a6d63b799c10dfb199
bfwuw-rsrkf <zy.130606@gmail.com>
2023-07-25 12:11:36 +0600

    added funcs categories (cats) and name_woman (name)

--- coolsite0/women/views.py
+++ coolsite0/women/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def index(request):
@@ -7,3 +8,11 @@ def index(request):
 
 def about(request):
     return render(request, "about.html")
+
+
+def categories(request, cats_id):
+    return HttpResponse(f"<h1>Статьи по категориям</h1> {cats_id}")
+
+
+def name_woman(request, name):
+    return HttpResponse(f"<h1>Женщину зовут {name}.</h1>")
