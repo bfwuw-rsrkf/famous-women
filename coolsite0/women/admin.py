af9070d5202d2b974ffa27f163c19496d46c535a
bfwuw-rsrkf <zy.130606@gmail.com>
2023-07-25 12:09:52 +0600

    created class WomenAdmin

--- coolsite0/women/admin.py
+++ coolsite0/women/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
-from women.models import Product, Woman
+from women.models import Woman
 
-admin.site.register(Product)
-admin.site.register(Woman)
+
+class WomenAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'content', 'photo', 'is_published', 'time_created', 'time_updated')
+    list_display_links = ('id', 'title')
+    list_editable = ('is_published',)
+    list_filter = ('is_published', 'time_created')
+    search_fields = ('title', 'content')
+
+
+admin.site.register(Woman, WomenAdmin)
