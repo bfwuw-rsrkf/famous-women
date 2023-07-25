f52d9139f2fc99e6e8c46ec2b797dd7cefdd619b
bfwuw-rsrkf <zy.130606@gmail.com>
2023-07-25 12:11:03 +0600

    added cats and name paths

--- coolsite0/women/urls.py
+++ coolsite0/women/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from women.views import index, about
+from women.views import index, about, categories, name_woman
 
 urlpatterns = [
     path('index/', index),
     path('about/', about),
+    path('cats/<int:cats_id>/', categories),
+    path('name/<str:name>/', name_woman)
 ]
