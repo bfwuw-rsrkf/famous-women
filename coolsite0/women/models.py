e46f426b21508df01f37ef60aef3c5fce1b60787
bfwuw-rsrkf <zy.130606@gmail.com>
2023-07-25 12:10:22 +0600

    changed class Woman, deleted class Product

--- coolsite0/women/models.py
+++ coolsite0/women/models.py
@@ -1,21 +1,13 @@
 from django.db import models
 
 
-class Product(models.Model):
-    model = models.CharField(max_length=256)
-    price = models.FloatField(default=0)
-    color = models.CharField(max_length=256)
-    date_created = models.DateField(auto_now_add=True)
-
-    def __str__(self):
-        return self.model
-
-
 class Woman(models.Model):
-    name = models.CharField(max_length=256)
-    age = models.IntegerField()
-    job = models.CharField(max_length=256)
-    salary = models.FloatField(default=0)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    photo = models.ImageField(upload_to="Images")
+    time_created = models.DateField(auto_now_add=True)
+    time_updated = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField()
 
     def __str__(self):
-        return self.name
+        return self.title
