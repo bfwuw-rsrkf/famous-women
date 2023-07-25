b3ebc7d5db857773f23ab9c4431c65eee7e37bdf
bfwuw-rsrkf <zy.130606@gmail.com>
2023-07-25 20:14:41 +0600

    added blank and null parameters to content and photo of class Woman

--- coolsite0/women/models.py
+++ coolsite0/women/models.py
@@ -3,11 +3,11 @@ from django.db import models
 
 class Woman(models.Model):
     title = models.CharField(max_length=255)
-    content = models.TextField()
-    photo = models.ImageField(upload_to="Images")
+    content = models.TextField(blank=True, null=True)
+    photo = models.ImageField(upload_to="Images", blank=True, null=True)
     time_created = models.DateField(auto_now_add=True)
     time_updated = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField()
+    is_published = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
