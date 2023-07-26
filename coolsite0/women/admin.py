cf8af81b3cfafc892c2161ddb6053ad3a4266b91
bfwuw-rsrkf <zy.130606@gmail.com>
2023-07-26 17:39:14 +0600

    renamed class WomenAdmin to WomanAdmin

--- coolsite0/women/admin.py
+++ coolsite0/women/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from women.models import Woman
 
 
-class WomenAdmin(admin.ModelAdmin):
+class WomanAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'content', 'photo', 'is_published', 'time_created', 'time_updated')
     list_display_links = ('id', 'title')
     list_editable = ('is_published', 'content')
@@ -10,4 +10,4 @@ class WomenAdmin(admin.ModelAdmin):
     search_fields = ('title', 'content')
 
 
-admin.site.register(Woman, WomenAdmin)
+admin.site.register(Woman, WomanAdmin)
