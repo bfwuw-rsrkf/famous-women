660dccb56535d77ef13d2eab3180b3165a8dafd2
bfwuw-rsrkf <zy.130606@gmail.com>
2023-07-26 17:36:11 +0600

    added 'content' to list_editable in class WomanAdmin

--- coolsite0/women/admin.py
+++ coolsite0/women/admin.py
@@ -5,7 +5,7 @@ from women.models import Woman
 class WomenAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'content', 'photo', 'is_published', 'time_created', 'time_updated')
     list_display_links = ('id', 'title')
-    list_editable = ('is_published',)
+    list_editable = ('is_published', 'content')
     list_filter = ('is_published', 'time_created')
     search_fields = ('title', 'content')
 
