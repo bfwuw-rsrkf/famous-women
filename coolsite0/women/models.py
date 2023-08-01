ad1c1916c8d6d437a5181be5537ced35cb994958
bfwuw-rsrkf <zy.130606@gmail.com>
2023-08-01 16:00:04 +0600

    created class Category, added attr cat (category) to class Woman

--- coolsite0/women/models.py
+++ coolsite0/women/models.py
@@ -1,4 +1,15 @@
 from django.db import models
+from django.urls import reverse
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('category', kwargs={'cat_id': self.pk})
 
 
 class Woman(models.Model):
@@ -8,6 +19,7 @@ class Woman(models.Model):
     time_created = models.DateField(auto_now_add=True)
     time_updated = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
+    cat = models.ForeignKey(Category, on_delete=models.PROTECT, null=True)
 
     def __str__(self):
         return self.title
