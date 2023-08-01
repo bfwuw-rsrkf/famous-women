1f60e496e247efcb26cd22b012fdcb06d28c3a54
bfwuw-rsrkf <zy.130606@gmail.com>
2023-08-01 15:59:25 +0600

    registered class Category

--- coolsite0/women/admin.py
+++ coolsite0/women/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
-from women.models import Woman
+from women.models import Woman, Category
 
 
 class WomanAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'content', 'photo', 'is_published', 'time_created', 'time_updated')
+    list_display = ('id', 'title', 'content', 'photo', 'is_published', 'time_created', 'time_updated', 'cat')
     list_display_links = ('id', 'title')
-    list_editable = ('is_published', 'content')
+    list_editable = ('is_published', 'content', 'cat')
     list_filter = ('is_published', 'time_created')
     search_fields = ('title', 'content')
 
 
 admin.site.register(Woman, WomanAdmin)
+admin.site.register(Category)
