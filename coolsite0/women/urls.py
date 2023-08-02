5d8b4d56d9e8b7a064035c9968cd1d885ad61046
bfwuw-rsrkf <zy.130606@gmail.com>
2023-08-02 20:29:58 +0600

    added post path

--- coolsite0/women/urls.py
+++ coolsite0/women/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from women.views import index, about, show_category
+from women.views import *
 
 urlpatterns = [
     path('index/', index, name='index'),
     path('about/', about),
     path('category/<int:cat_id>/', show_category, name='category'),
+    path('post/<slug:post_slug>', show_post, name='post')
 ]
