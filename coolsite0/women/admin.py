1b70606b611fb1e1057a04dff4f54a9b8242afe0
bfwuw-rsrkf <zy.130606@gmail.com>
2023-08-02 20:28:32 +0600

    created class CategoryAdmin

--- coolsite0/women/admin.py
+++ coolsite0/women/admin.py
@@ -3,12 +3,20 @@ from women.models import Woman, Category
 
 
 class WomanAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'content', 'photo', 'is_published', 'time_created', 'time_updated', 'cat')
+    list_display = ('id', 'title', 'time_created', 'photo', 'is_published')
     list_display_links = ('id', 'title')
-    list_editable = ('is_published', 'content', 'cat')
-    list_filter = ('is_published', 'time_created')
     search_fields = ('title', 'content')
+    list_editable = ('is_published',)
+    list_filter = ('is_published', 'time_created')
+    prepopulated_fields = {"slug": ("title",)}
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+    prepopulated_fields = {"slug": ("name",)}
 
 
 admin.site.register(Woman, WomanAdmin)
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
