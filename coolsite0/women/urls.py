9d7d87e349ba68ecded9d564588e252929d1c497
bfwuw-rsrkf <zy.130606@gmail.com>
2023-08-04 22:24:02 +0600

    importing from .views, changed index, post and add_page paths (now they use classes)

--- coolsite0/women/urls.py
+++ coolsite0/women/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from women.views import *
+from .views import *
 
 urlpatterns = [
-    path('index/', index, name='index'),
+    path('index/', HomePageListView.as_view(), name='index'),
     path('about/', about, name='about'),
     path('category/<int:cat_id>/', show_category, name='category'),
-    path('post/<slug:post_slug>', show_post, name='post'),
-    path('add_page', add_page, name='add_page'),
+    path('post/<slug:post_slug>', ShowPostDetailView.as_view(), name='post'),
+    path('add_page', AddPageCreateView.as_view(), name='add_page'),
     path('contact', contact, name='contact'),
     path('login', login, name='login'),
 ]
