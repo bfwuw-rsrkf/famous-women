1a07f00afc00dc11e33746d56c1e748e8269a00f
bfwuw-rsrkf <zy.130606@gmail.com>
2023-08-04 15:42:45 +0600

    created post, add_page, contact and login paths

--- coolsite0/women/urls.py
+++ coolsite0/women/urls.py
@@ -3,7 +3,10 @@ from women.views import *
 
 urlpatterns = [
     path('index/', index, name='index'),
-    path('about/', about),
+    path('about/', about, name='about'),
     path('category/<int:cat_id>/', show_category, name='category'),
-    path('post/<slug:post_slug>', show_post, name='post')
+    path('post/<slug:post_slug>', show_post, name='post'),
+    path('add_page', add_page, name='add_page'),
+    path('contact', contact, name='contact'),
+    path('login', login, name='login'),
 ]
