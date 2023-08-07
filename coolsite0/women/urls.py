46def14a61ccee6cda2751e078eea1520824f382
bfwuw-rsrkf <zy.130606@gmail.com>
2023-08-07 21:10:06 +0600

    category path now uses slug

--- coolsite0/women/urls.py
+++ coolsite0/women/urls.py
@@ -4,9 +4,9 @@ from .views import *
 urlpatterns = [
     path('index/', HomePageListView.as_view(), name='index'),
     path('about/', about, name='about'),
-    path('category/<int:cat_id>/', show_category, name='category'),
-    path('post/<slug:post_slug>', ShowPostDetailView.as_view(), name='post'),
-    path('add_page', AddPageCreateView.as_view(), name='add_page'),
-    path('contact', contact, name='contact'),
-    path('login', login, name='login'),
+    path('category/<slug:cat_slug>/', ShowCategoryListView.as_view(), name='category'),
+    path('post/<slug:post_slug>/', ShowPostDetailView.as_view(), name='post'),
+    path('add_page/', AddPageCreateView.as_view(), name='add_page'),
+    path('contact/', contact, name='contact'),
+    path('login/', login, name='login'),
 ]
