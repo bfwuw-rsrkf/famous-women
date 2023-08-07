4e16d1d2ec3ca2a89345d880769fa71d7d689036
bfwuw-rsrkf <zy.130606@gmail.com>
2023-08-07 21:14:02 +0600

    replaced show_category func with the corresponding class and all view classes are now inherited from DataMixin

--- coolsite0/women/views.py
+++ coolsite0/women/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render  # , redirect, get_object_or_404
 from django.views.generic import ListView, DetailView, CreateView
 from django.urls import reverse_lazy
+from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Woman
 from .forms import AddPostForm
+from .utils import DataMixin
 
 
 # CRUD request - create, retrieve, update, delete
@@ -21,28 +23,15 @@ menu = [
 ]
 
 
-# def index(request):
-#     posts = Woman.objects.filter(is_published=True)
-#     data = {
-#         'title': 'Главная страница',
-#         'menu': menu,
-#         'posts': posts,
-#         'cat_selected': 0,
-#     }
-#     return render(request, 'index.html', data)
-
-
-class HomePageListView(ListView):
+class HomePageListView(LoginRequiredMixin, DataMixin, ListView):
     model = Woman
     template_name = 'index.html'
     context_object_name = 'posts'
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Главная страница'
-        context['menu'] = menu
-        context['cat_selected'] = 0
-        return context
+        c_def = self.get_user_context(title='Главная страница')
+        return dict(list(context.items()) + list(c_def.items()))
 
     def get_queryset(self):
         return Woman.objects.filter(is_published=True)
@@ -53,29 +42,24 @@ def about(request):
     return render(request, 'index.html', data)
 
 
-def show_category(request, cat_id):
-    posts = Woman.objects.filter(cat_id=cat_id, is_published=True)
-    context = {
-        'title': 'Отображение по рубрикам',
-        'menu': menu,
-        'posts': posts,
-        'cat_selected': cat_id,
-    }
-    return render(request, 'index.html', context=context)
+class ShowCategoryListView(DataMixin, ListView):
+    model = Woman
+    template_name = 'index.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        return Woman.objects.filter(cat__slug=self.kwargs['cat_slug'], is_published=True)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(
+            title=f'Категория - {str(context["posts"][0].cat)}',
+            cat_selected=context['posts'][0].cat_id
+        )
+        return dict(list(context.items()) + list(c_def.items()))
 
 
-# def show_post(request, post_slug):
-#     post = get_object_or_404(Woman, slug=post_slug)
-#     context = {
-#         "title": 'Главная страница',
-#         "menu": menu,
-#         "post": post,
-#         "cat_selected": post.cat_id,
-#     }
-#     return render(request, "post.html", context=context)
-
-
-class ShowPostDetailView(DetailView):
+class ShowPostDetailView(DataMixin, DetailView):
     model = Woman
     template_name = 'post.html'
     context_object_name = 'post'
@@ -83,34 +67,19 @@ class ShowPostDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = context['post']
-        context['menu'] = menu
-        context['cat_selected'] = context['post'].cat_id
-        return context
+        c_def = self.get_user_context(title=context['posts'])
+        return dict(list(context.items()) + list(c_def.items()))
 
 
-# def add_page(request):
-#     if request.method == 'POST':
-#         form = AddPostForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('index')
-#     else:
-#         form = AddPostForm
-#     context = {'form': form, 'menu': menu, 'title': 'Добавление статьи'}
-#     return render(request, 'add_page.html', context=context)
-
-
-class AddPageCreateView(CreateView):
+class AddPageCreateView(DataMixin, CreateView):
     form_class = AddPostForm
     template_name = 'add_page.html'
     success_url = reverse_lazy('index')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Добавить статью'
-        context['menu'] = menu
-        return context
+        c_def = self.get_user_context(title='Добавление статьи')
+        return dict(list(context.items()) + list(c_def.items()))
 
 
 def contact(request):
