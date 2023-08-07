3b492e674908099a78833e6e8b9e42881cd1b49b
bfwuw-rsrkf <zy.130606@gmail.com>
2023-08-07 21:09:24 +0600

    changed get_absolute_url func in model Category

--- coolsite0/women/models.py
+++ coolsite0/women/models.py
@@ -10,7 +10,7 @@ class Category(models.Model):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('category', kwargs={'cat_id': self.pk})
+        return reverse('category', kwargs={'cat_slug': self.slug})
 
 
 class Woman(models.Model):
