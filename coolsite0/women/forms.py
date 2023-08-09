96ed43b9008a9c63edc1828ed1f2263e1ad3ac46
bfwuw-rsrkf <zy.130606@gmail.com>
2023-08-09 15:55:28 +0600

    added registration and login forms

--- coolsite0/women/forms.py
+++ coolsite0/women/forms.py
@@ -1,13 +1,7 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
 from .models import Woman
-
-
-# class AddPostForm(forms.Form):
-#     title = forms.CharField(max_length=255, label='Имя')
-#     cat = forms.ModelChoiceField(queryset=Category.objects.all(), label='Категория', empty_label='Не выбрано')
-#     content = forms.CharField(label='Биография')
-#     slug = forms.SlugField(max_length=255)
-#     is_published = forms.BooleanField(required=False, label='Опубликовать')
 
 
 class AddPostForm(forms.ModelForm):
@@ -20,3 +14,24 @@ class AddPostForm(forms.ModelForm):
             'slug',
             'is_published'
         ]
+
+
+class RegisterUserForm(UserCreationForm):
+    username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-input'}))
+    email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class': 'form-input'}))
+    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    password2 = forms.CharField(label='Повтор пароля', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'email',
+            'password1',
+            'password2'
+        ]
+
+
+class LoginUserForm(AuthenticationForm):
+    username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-input'}))
+    password = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
