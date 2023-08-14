32968d30c8d74339106a0b401333a3d3d9d040ed
bfwuw-rsrkf <zy.130606@gmail.com>
2023-08-14 23:15:39 +0600

    added get_html_photo func for WomanAdmin, changed header for admin app

--- coolsite0/women/admin.py
+++ coolsite0/women/admin.py
@@ -1,14 +1,19 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 from .models import Woman, Category
 
 
 class WomanAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'time_created', 'photo', 'is_published')
+    list_display = ('id', 'title', 'time_created', 'get_html_photo', 'is_published')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
     list_editable = ('is_published',)
     list_filter = ('is_published', 'time_created')
     prepopulated_fields = {"slug": ("title",)}
+
+    def get_html_photo(self, object):
+        if object.photo:
+            return mark_safe(f"<img src='{object.photo.url}' width=50>")
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -20,3 +25,5 @@ class CategoryAdmin(admin.ModelAdmin):
 
 admin.site.register(Woman, WomanAdmin)
 admin.site.register(Category, CategoryAdmin)
+
+admin.site.site_header = 'Админ-панель сайта о женщинах'
